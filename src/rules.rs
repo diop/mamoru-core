67f387418a11adc61544a5c4063f4d3c21e73f5b
Viktoriia <vikaboychuk93@gmail.com>
2022-09-20 11:28:25 -0400

    MAMORU-17: Create Rules manager

--- src/rules.rs
+++ src/rules.rs
@@ -57,3 +57,43 @@ pub struct Rule {
     conditions: Condition,
     actions: Vec<ActionType>,
 }
+
+impl Rule {
+    pub fn new_rule(chain: Chain, conditions: Condition, actions: Vec<ActionType>) -> Self {
+        Rule { chain, conditions, actions }
+    }
+
+    pub fn get_rule_chain(&self) -> &Chain {
+        &self.chain
+    }
+
+    pub fn get_rule_conditions(&self) -> &Condition {
+        &self.conditions
+    }
+
+    pub fn get_rule_actions(&self) -> &Vec<ActionType> {
+        &self.actions
+    }
+}
+
+pub type RulesList = Vec<Rule>;
+
+trait RulesManager {
+    fn create_rule(&mut self, chain: Chain, conditions: Condition, actions: Vec<ActionType>) -> &Rule;
+    fn remove_rule(&mut self, rule_id: usize) -> Option<Rule>;
+}
+
+impl RulesManager for RulesList {
+    fn create_rule(&mut self, chain: Chain, conditions: Condition, actions: Vec<ActionType>) -> &Rule {
+        let rule = Rule::new_rule(chain, conditions, actions);
+        self.push(rule);
+        self.last().unwrap()
+    }
+
+    fn remove_rule(&mut self, rule_id: usize) -> Option<Rule> {
+        match self.get(rule_id) {
+            Some(_) => Some(self.remove(rule_id)),
+            None => None
+        }
+    }
+}
